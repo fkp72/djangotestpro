ee31561ebe452c13c32e3e72fbcabc28e6fdfdd3
fkp72 <52627524+fkp72@users.noreply.github.com>
2023-12-18 23:01:50 +0530

    newvalue views.py

--- my_app/views.py
+++ my_app/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Hello, welcome to this sample webpage built using django')
+    return HttpResponse('Hello, welcome to My sample webpage built using django')
 
